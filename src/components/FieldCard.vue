b769cf739aeb011fafba74158a9545770f8591d1
Alexander Kuznetsov <geff21st@gmail.com>
2021-01-04 01:03:36 +0300

    Добавлена возможность управлять состоянием клеток (агент / cчетчик)

--- src/components/FieldCard.vue
+++ src/components/FieldCard.vue
@@ -10,7 +10,15 @@
           black: word.killerCurrent === true
         }"
         :data-index="i"
+        @click="switchState(i)"
       >
+        <span v-if="isCellAgent(i)" class="card-cell-agent">
+          <img :src="agentsAvailable[i % agentsAvailable.length]" :alt="'Агент #' + i">
+        </span>
+
+        <span v-if="isCellArrowDown(i)" class="card-cell-arrow card-cell-arrow--down">\/</span>
+        <span v-if="isCellArrowUp(i)" class="card-cell-arrow card-cell-arrow--up">/\</span>
+
         <span v-text="word.text" class="word"></span>
       </div>
     </div>
@@ -18,15 +26,57 @@
 </template>
 
 <script>
+const CELL_STATE_NONE = 0
+const CELL_STATE_AGENT = 10
+const CELL_STATE_ARROW_UP = 20
+const CELL_STATE_ARROW_DOWN = 30
+
 export default {
   name: 'FieldCard',
   data () {
     return {
-      size: 5
+      size: 5,
+      agentsAvailable: [
+        'img/agent-1.jpg',
+        'img/agent-2.jpg',
+        'img/agent-3.jpg',
+        'img/agent-4.jpg'
+      ],
+      cellStates: {},
+      availableStates: [
+        CELL_STATE_NONE,
+        CELL_STATE_AGENT,
+        CELL_STATE_ARROW_UP,
+        CELL_STATE_ARROW_DOWN
+      ]
     }
   },
   props: {
     words: Array
+  },
+  methods: {
+    getCellState (index) {
+      const cellState = this.cellStates[index] || 0
+      return this.availableStates[cellState] || this.availableStates[0]
+    },
+    isCellAgent (index) {
+      return this.getCellState(index) === CELL_STATE_AGENT
+    },
+    isCellArrowDown (index) {
+      return this.getCellState(index) === CELL_STATE_ARROW_DOWN
+    },
+    isCellArrowUp (index) {
+      return this.getCellState(index) === CELL_STATE_ARROW_UP
+    },
+    switchState (index) {
+      let cellState = this.cellStates[index] || 0
+
+      cellState = (cellState + 1) % this.availableStates.length
+
+      if (!this.availableStates[cellState]) cellState = 0
+
+      this.$set(this.cellStates, index, cellState)
+    }
   }
 }
 </script>
@@ -59,6 +109,7 @@ export default {
   }
 
   &-cell {
+    cursor: pointer;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -67,12 +118,14 @@ export default {
     border-radius: 14px;
     box-shadow: inset 0 0 3px #00000055;
     position: relative;
-    text-shadow: 0 0 2px #fff;
+    text-shadow: 0 1px 0 #ffffffcc;
+    color: #222;
+    overflow: hidden;
 
     &.green {
       background-color: #82b34a;
       color: #fff;
-      text-shadow: 0 0 2px #000;
+      text-shadow: 0 1px 1px #000000aa;
     }
 
     &.red {
@@ -87,17 +140,21 @@ export default {
     }
 
     &.black {
-      background-color: #000;
+      background-color: #222;
       color: #fff;
+      text-shadow: none;
     }
   }
 
   .word {
+    pointer-events: none;
+    user-select: none;
     display: flex;
     margin: auto;
     font-size: 13px;
     //letter-spacing: -0.9px;
     justify-content: center;
+    font-weight: bold;
     align-items: center;
     text-align: center;
     font-family: Arial, "Helvetica Neue", Helvetica, sans-serif;
@@ -110,6 +167,43 @@ export default {
     @at-root html[data-ratio=vertical] #{&} {
       transform: rotate(-65deg);
     }
+  }
+}
+
+.card-cell-agent {
+  z-index: 100;
+  opacity: .6;
+
+  &, img {
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+  }
+
+  img {
+    object-fit: cover;
+    object-position: top center;
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.card-cell-arrow {
+  position: absolute;
+  font-size: 20px;
+  font-family: monospace;
+  letter-spacing: -4.2px;
+  font-weight: bolder;
+
+  &--up {
+    top: 8px;
+  }
+
+  &--down {
+    bottom: 8px;
   }
 }
 </style>
